93457d18e2b92e5de5f7d62acf7177fb7a204483
DaryanZabear <71202670+Daiyan-Zubaier@users.noreply.github.com>
2025-04-26 13:53:14 -0400

    Better tunnel API use

--- web-code/rain-sense/vite.config.ts
+++ web-code/rain-sense/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const TUNNEL = env.VITE_TUNNEL_URL;
+
+  return {
+    server: {
+      host: '::',
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: TUNNEL,
+          changeOrigin: true,
+          secure: true,
+        },
+      },
+    },
+  };
+});

51b585762368b3ef9da35c0e154564ccbd8684d7
DaryanZabear <71202670+Daiyan-Zubaier@users.noreply.github.com>
2025-04-26 14:00:42 -0400

    Re-add app_main.c fresh

--- main/incs/wifi_manager.h
+++ main/incs/wifi_manager.h
@@ -1,6 +1,9 @@
 #ifndef WIFI_MANAGER_H
 #define WIFI_MANAGER_H
 
+#define WIFI_SSID "Wifi SSID"
+#define WIFI_PASSWORD "Wifi Password"
+
 /* Initialise STA mode and block until connected (or reboot on failure) */
 void wifi_init_sta(const char *ssid, const char *pass);
 
